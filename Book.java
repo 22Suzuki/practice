b66c23c908da05522f96b9c8e725985d4fc55d36
22Suzuki <22suzu22kia@gmail.com>
2023-07-18 18:37:18 +0900

    Crate Book.java

---
+++ Book.java
@@ -0,0 +1,8 @@
+public class Bool extends TangibleAsset{
+	private String isbn;
+	public Book (String name,int price, String color, String isbn){
+		super(name,price,color);
+		this.isbn = isbn;
+	}
+	public String getlsbn( ){ return this.isbn ;}
+}
